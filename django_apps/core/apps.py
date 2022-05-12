c40bf3d5bad90ab12e30657ba18832fa2a330d73
Dani Hodovic <dani.hodovic@gmail.com>
2022-05-12 18:10:24 +0200

    Import signals in core/apps.py

--- django_apps/core/apps.py
+++ django_apps/core/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_apps.core"
+
+    def ready(self):
+        try:
+            # pylint: disable=unused-import,import-outside-toplevel
+            import django_apps.core.signals
+        except ImportError:
+            pass
