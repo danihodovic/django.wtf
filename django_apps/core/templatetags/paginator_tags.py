515cbc092f219cdc26725996e8962888f9043f63
Adin Hodovic <hodovicadin@gmail.com>
2022-05-20 21:26:43 +0200

    fix: Mypy errors

--- django_apps/core/templatetags/paginator_tags.py
+++ django_apps/core/templatetags/paginator_tags.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.simple_tag
 def get_proper_elided_page_range(p, number, on_each_side=3, on_ends=2):
     paginator = Paginator(p.object_list, p.per_page)
-    return paginator.get_elided_page_range(
+    return paginator.get_elided_page_range(  # type: ignore
         number=number, on_each_side=on_each_side, on_ends=on_ends
     )
