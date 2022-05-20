10f2c017f525173d3a4ac8740108c924b58ab1d4
Adin Hodovic <hodovicadin@gmail.com>
2022-05-20 17:10:18 +0200

    feat: Update styling

--- django_apps/theme/static_src/tailwind.config.js
+++ django_apps/theme/static_src/tailwind.config.js
@@ -58,7 +58,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
         require('daisyui'),
     ],
