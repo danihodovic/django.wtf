a21a3f6619e31f27b188bd1f17ee8d607bbeb1a6
Adin Hodovic <hodovicadin@gmail.com>
2022-05-20 20:00:43 +0200

    feat: Remove dark theme

--- django_apps/theme/static_src/tailwind.config.js
+++ django_apps/theme/static_src/tailwind.config.js
@@ -62,4 +62,16 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('daisyui'),
     ],
+  daisyui: {
+    styled: true,
+    themes: [
+      'light'
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
 }
