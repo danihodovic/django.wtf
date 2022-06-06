744851121a755bfca253dbd55d6218f221ee1b49
Dani Hodovic <dani.hodovic@gmail.com>
2022-06-06 16:55:43 +0200

    Add admin filter to see if repo has indexed README.md

---
+++ django_apps/core/filters.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+
+class HasReadmeListFilter(admin.SimpleListFilter):
+    title = "has indexed README"
+    parameter_name = "has_readme"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("true", "yes"),
+            ("false", "no"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "true":
+            return queryset.filter(readme_html__isnull=False)
+        return queryset.filter(readme_html__isnull=True)
