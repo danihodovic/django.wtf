986dd83b48e4b62a7d51a893c1fb7c50bb50125b
Dani Hodovic <dani.hodovic@gmail.com>
2022-11-16 06:55:54 +0100

    fix: set CSRF_TRUSTED_ORIGINS to fix Django 4.0

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["djangoapps.dev"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django.wtf"])
+# https://docs.djangoproject.com/en/4.0/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 # DATABASES
 # ------------------------------------------------------------------------------
