1ec52246872410555b00b2d7aadadc70b589a3c2
Dani Hodovic <dani.hodovic@gmail.com>
2022-12-06 12:56:24 +0100

    fix: prefix CSRF_TRUSTED_ORIGINS with https

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django.wtf"])
 # https://docs.djangoproject.com/en/4.0/ref/settings/#csrf-trusted-origins
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = [f"https://{h}" for h in ALLOWED_HOSTS]
 
 # DATABASES
 # ------------------------------------------------------------------------------
