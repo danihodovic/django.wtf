dcd80e80f0f15a164960a28fadd83449e4078e4d
Dani Hodovic <dani.hodovic@gmail.com>
2023-05-13 17:37:46 +0200

    fix: remove django-import-export
    Fails builds due to dependency on markuppy.

--- django_apps/core/admin.py
+++ django_apps/core/admin.py
@@ -1,7 +1,5 @@
 # pylint: disable=too-few-public-methods
 from django.contrib import admin
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
 from watson.admin import SearchAdmin
 
 from .filters import HasReadmeListFilter
@@ -17,7 +15,7 @@ from .models import (
 
 
 @admin.register(Category)
-class CategoryAdmin(ImportExportModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "emoji")
     search_fields = ("name",)
 
@@ -32,6 +30,7 @@ class RepositoryAdmin(SearchAdmin):
         "topics",
         "type",
         "created",
+        "modified",
     )
     list_filter = ("type", HasReadmeListFilter)
     search_fields = (
@@ -49,7 +48,7 @@ class RepositoryAdmin(SearchAdmin):
 
 
 @admin.register(RepositoryStars)
-class RepositoryStarsAdmin(ImportExportModelAdmin):
+class RepositoryStarsAdmin(admin.ModelAdmin):
     list_display = ("repository", "created_at", "stars")
     list_filter = ()
     search_fields = ("repository__full_name",)
@@ -77,7 +76,7 @@ class ProfileAdmin(SearchAdmin):
 
 
 @admin.register(ProfileFollowers)
-class ProfileFollowersAdmin(ImportExportModelAdmin):
+class ProfileFollowersAdmin(admin.ModelAdmin):
     list_display = ("profile", "created_at", "followers")
     list_filter = ()
     search_fields = ("profile__login",)
@@ -103,46 +102,3 @@ class PypiProjectAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         return [f.name for f in self.model._meta.fields]
-
-
-class ProfileResource(resources.ModelResource):
-    class Meta:
-        model = Profile
-        instance_loader = "import_export.instance_loaders.CachedInstanceLoader"
-        skip_diff = True
-        use_bulk = True
-
-
-class CategoryResource(resources.ModelResource):
-    class Meta:
-        model = Category
-        instance_loader = "import_export.instance_loaders.CachedInstanceLoader"
-        skip_diff = True
-        use_bulk = True
-
-
-class RepositoryResource(resources.ModelResource):
-    class Meta:
-        model = Repository
-        instance_loader = "import_export.instance_loaders.CachedInstanceLoader"
-        skip_diff = True
-        use_bulk = True
-
-
-class RepositoryStarsResource(resources.ModelResource):
-    class Meta:
-        model = RepositoryStars
-        instance_loader = "import_export.instance_loaders.CachedInstanceLoader"
-        skip_diff = True
-        use_bulk = True
-
-    def get_queryset(self):
-        return super().get_queryset().select_related("repository")
-
-
-class SocialNewsResource(resources.ModelResource):
-    class Meta:
-        model = SocialNews
-        instance_loader = "import_export.instance_loaders.CachedInstanceLoader"
-        skip_diff = True
-        use_bulk = True
