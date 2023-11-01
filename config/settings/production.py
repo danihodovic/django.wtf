4236d2b0e47ded7ca8076f47954e64f4930283af
adinhodovic <hodovicadin@gmail.com>
2023-11-01 15:35:01 +0100

    fix: Add db engine to prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,6 +14,7 @@ CSRF_TRUSTED_ORIGINS = [f"https://{h}" for h in ALLOWED_HOSTS]
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES["default"]["ENGINE"] = "django_prometheus.db.backends.postgresql"
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
